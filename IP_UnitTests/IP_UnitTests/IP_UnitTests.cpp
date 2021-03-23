af780866173155f1b65147f3eb3887aeb2cd1cf4
7ydini <61342487+7ydini@users.noreply.github.com>
2021-03-23 10:55:51 +0300

    Update IP_UnitTests.cpp

--- IP_UnitTests/IP_UnitTests/IP_UnitTests.cpp
+++ IP_UnitTests/IP_UnitTests/IP_UnitTests.cpp
@@ -13,7 +13,7 @@ namespace IPUnitTests
 	TEST_CLASS(NoneSlove) // 0
 	{
 	public:
-		TEST_METHOD(NoneSloves)
+		TEST_METHOD(NoneSloves1)
 		{
 			string resoult;
 			string funResoult;
@@ -22,12 +22,42 @@ namespace IPUnitTests
 			Assert res;
 			res.AreEqual(resoult, funResoult);
 		}
+
+		TEST_METHOD(NoneSloves2)
+		{
+			string resoult;
+			string funResoult;
+			resoult = "0";
+			funResoult = fun(0, 1, 0, 1, 0, 1);
+			Assert res;
+			res.AreEqual(resoult, funResoult);
+		}
+		TEST_METHOD(NoneSloves3)
+		{
+			string resoult;
+			string funResoult;
+			resoult = "0";
+			funResoult = fun(0, 0, 0, 1, 1, 1);
+			Assert res;
+			res.AreEqual(resoult, funResoult);
+		}
+
+		TEST_METHOD(NoneSloves4)
+		{
+			string resoult;
+			string funResoult;
+			resoult = "0";
+			funResoult = fun(0, 0, 0, 0, 1, 1);
+			Assert res;
+			res.AreEqual(resoult, funResoult);
+		}
+
 	};
 
 	TEST_CLASS(InfSloveK) {		// 1 y=kx+n
 public:
 
-	TEST_METHOD(InfSlovesK1)
+	TEST_METHOD(InfSlovesBandE)
 	{
 		string resoult;
 		string funResoult;
@@ -37,7 +67,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesK2)
+	TEST_METHOD(InfSlovesAandE)
 	{
 		string resoult;
 		string funResoult;
@@ -46,7 +76,7 @@ public:
 		Assert res;
 		res.AreEqual(resoult, funResoult);
 	}
-	TEST_METHOD(InfSlovesK3)
+	TEST_METHOD(InfSlovesDandF)
 	{
 		string resoult;
 		string funResoult;
@@ -56,7 +86,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesK4)
+	TEST_METHOD(InfSlovesCandF)
 	{
 		string resoult;
 		string funResoult;
@@ -65,7 +95,7 @@ public:
 		Assert res;
 		res.AreEqual(resoult, funResoult);
 	}
-	TEST_METHOD(InfSlovesK5)
+	TEST_METHOD(InfSlovesABCD)
 	{
 		string resoult;
 		string funResoult;
@@ -75,7 +105,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesK6)
+	TEST_METHOD(InfSlovesABCD2)
 	{
 		string resoult;
 		string funResoult;
@@ -98,6 +128,15 @@ public:
 		Assert res;
 		res.AreEqual(resoult, funResoult);
 	}
+	TEST_METHOD(OnlyOneWay12)
+	{
+		string resoult;
+		string funResoult;
+		resoult = "2 " + to_string(double(1)) + " -" + to_string(double(0));
+		funResoult = fun(0, 1, 1, 0, 0, 1);
+		Assert res;
+		res.AreEqual(resoult, funResoult);
+	}
 	TEST_METHOD(OnlyOneWay2)
 	{
 		string resoult;
@@ -108,11 +147,21 @@ public:
 		res.AreEqual(resoult, funResoult);
 
 	}
+	TEST_METHOD(OnlyOneWay22)
+	{
+		string resoult;
+		string funResoult;
+		resoult = "2 -" + to_string(double(0)) + " " + to_string(double(1));
+		funResoult = fun(0, 1, 1, 0, 1, 0);
+		Assert res;
+		res.AreEqual(resoult, funResoult);
+
+	}
 	};
 
 	TEST_CLASS(InfSloveX) {		//	3 x0
 public:
-	TEST_METHOD(InfSlovesX1)
+	TEST_METHOD(InfSlovesBandD)
 	{
 		string resoult;
 		string funResoult;
@@ -122,7 +171,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesX2)
+	TEST_METHOD(InfSlovesBandD2)
 	{
 		string resoult;
 		string funResoult;
@@ -132,7 +181,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesX3)
+	TEST_METHOD(InfSlovesBandD3)
 	{
 		string resoult;
 		string funResoult;
@@ -142,7 +191,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesX4)
+	TEST_METHOD(InfSlovesBandD4)
 	{
 		string resoult;
 		string funResoult;
@@ -151,10 +200,31 @@ public:
 		Assert res;
 		res.AreEqual(resoult, funResoult);
 	}
+
+	TEST_METHOD(InfSlovesA)
+	{
+		string resoult;
+		string funResoult;
+		resoult = "3 " + to_string(double(0));
+		funResoult = fun(1, 0, 0, 0, 0, 0);
+		Assert res;
+		res.AreEqual(resoult, funResoult);
+	}
+
+	TEST_METHOD(InfSlovesC)
+	{
+		string resoult;
+		string funResoult;
+		resoult = "3 " + to_string(double(0));
+		funResoult = fun(0, 0, 1, 0, 0, 0);
+		Assert res;
+		res.AreEqual(resoult, funResoult);
+	}
+
 	};
 	TEST_CLASS(InfSloveY) {		// 4 y0
 public:
-	TEST_METHOD(InfSlovesY)
+	TEST_METHOD(InfSlovesAandC)
 	{
 		string resoult;
 		string funResoult;
@@ -164,7 +234,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesY2)
+	TEST_METHOD(InfSlovesAandC2)
 	{
 		string resoult;
 		string funResoult;
@@ -174,7 +244,7 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesY3)
+	TEST_METHOD(InfSlovesAandC3)
 	{
 		string resoult;
 		string funResoult;
@@ -184,12 +254,32 @@ public:
 		res.AreEqual(resoult, funResoult);
 	}
 
-	TEST_METHOD(InfSlovesY4)
+	TEST_METHOD(InfSlovesAandC4)
 	{
 		string resoult;
 		string funResoult;
 		resoult = "4 " + to_string(double(1));
 		funResoult = fun(0, 0, 0, 1, 0, 1);
+		Assert res;
+		res.AreEqual(resoult, funResoult);
+	}
+
+	TEST_METHOD(InfSlovesD)
+	{
+		string resoult;
+		string funResoult;
+		resoult = "4 " + to_string(double(0));
+		funResoult = fun(0, 0, 0, 1, 0, 0);
+		Assert res;
+		res.AreEqual(resoult, funResoult);
+	}
+
+	TEST_METHOD(InfSlovesB)
+	{
+		string resoult;
+		string funResoult;
+		resoult = "4 " + to_string(double(0));
+		funResoult = fun(0, 1, 0, 0, 0, 0);
 		Assert res;
 		res.AreEqual(resoult, funResoult);
 	}
